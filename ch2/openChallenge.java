4221d924cb2ba5e306a5f2267dc737353dadfc8f
eoxing <dbstjwls1002@gmail.com>
2023-01-03 10:24:17 +0900

    230103

---
+++ ch2/openChallenge.java
@@ -0,0 +1,46 @@
+package ch2;
+
+import java.util.*;
+
+public class openChallenge {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String cheolsoo;
+		String younghee;
+		
+		System.out.println("가위바위보 게임입니다. 가위, 바위, 보 중에서 입력하세요");
+		System.out.print("철수 >> ");
+		cheolsoo = sc.next();
+		System.out.print("영희 >> ");
+		younghee = sc.next();
+		
+		if(cheolsoo.equals("가위")) {
+			if(younghee.equals("보"))
+				System.out.println("철수가 이겼습니다.");
+			else if(younghee.equals("가위"))
+				System.out.println("비겼습니다.");
+			else
+				System.out.println("영희가 이겼습니다.");
+		}
+		
+		else if(cheolsoo.equals("바위")) {
+			if(younghee.equals("바위"))
+				System.out.println("비겼습니다.");
+			else if(younghee.equals("가위"))
+				System.out.println("철수가 이겼습니다.");
+			else
+				System.out.println("영희가 이겼습니다");
+		}
+		
+		else if(cheolsoo.equals("보")) {
+			if(younghee.equals("가위"))
+				System.out.println("영희가 이겼습니다.");
+			else if(younghee.equals("바위"))
+				System.out.println("철수가 이겼습니다.");
+			else
+				System.out.println("비겼습니다.");
+		}
+	}
+
+}
